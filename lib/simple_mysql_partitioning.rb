9fb973a0392e059527c78177e81cc965afa9f2cf
Shunsuke Andoh <shunsuke.andoh@gmail.com>
2018-07-23 02:41:50 +0900

    [feat] add require executor

--- lib/simple_mysql_partitioning.rb
+++ lib/simple_mysql_partitioning.rb
@@ -1,6 +1,8 @@
 require 'simple_mysql_partitioning/version'
+require 'active_record'
+require 'mysql2'
 require 'simple_mysql_partitioning/sql'
-#require 'simple_mysql_partitioning/executor'
+require 'simple_mysql_partitioning/executor'
 
 module SimpleMysqlParitioning
   # Your code goes here...
