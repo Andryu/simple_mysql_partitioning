93c404c0ca3541fd3a3097320eb4dc87346a857e
Shunsuke Andoh <shunsuke.andoh@gmail.com>
2018-07-23 00:35:21 +0900

    [feat] require excutor

--- lib/simple_mysql_paritioning.rb
+++ lib/simple_mysql_paritioning.rb
@@ -1,5 +1,6 @@
-require "simple_mysql_paritioning/version"
-require "simple_mysql_paritioning/sql"
+require 'simple_mysql_paritioning/version'
+require 'simple_mysql_paritioning/sql'
+#require 'simple_mysql_paritioning/executor'
 
 module SimpleMysqlParitioning
   # Your code goes here...
