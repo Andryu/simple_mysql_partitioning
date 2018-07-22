ce8d60734eebf8c6e49007290df4b2ad5ef8c21f
Shunsuke Andoh <shunsuke.andoh@gmail.com>
2018-07-23 00:33:04 +0900

    [add] Add active record excuteor

---
+++ lib/simple_mysql_paritioning/executor.rb
@@ -0,0 +1,37 @@
+
+require 'active_record'
+
+module SimpleMySQLPartitioning
+  class Executor
+    def initialize(klass, partition_name)
+      @klass = klass
+      @table_name = @klass.table_name
+      @partition_name = partition_name
+    end
+
+    def add(less_than_value)
+      add_partition_sql = add_sql(@table_name, @partition_name, less_than_value)
+      @klass.connection.execute(add_partition_sql)
+    end
+
+    def reorganize(less_than_value, reorganize_partition_name)
+      @klass.connection.execute(
+        reorganize_sql(
+          @table_name,
+          @partition_name, less_than_value,
+          reorganize_partition_name
+        )
+      )
+    end
+
+    def exist?
+      @klass.connection.select_all(
+        exist_sql(@table_name, @partition_name)
+      ).to_hash.present?
+    end
+
+    def drop
+      @klass.connection.execute(parge_sql(@table_name, @partition_name))
+    end
+  end
+end
