db4a295a7d09e3f6bdd724ffaa6205bdcfa243ab
Shunsuke Andoh <shunsuke.andoh@gmail.com>
2018-07-24 12:13:04 +0900

    [spec] add以外のメソッドテスト

--- spec/simple_mysql_partitioning/executor_spec.rb
+++ spec/simple_mysql_partitioning/executor_spec.rb
@@ -1,7 +1,34 @@
 require 'spec_helper'
 
+class DailyReport < ActiveRecord::Base
+  self.table_name = 'daily_reports'
+end
+
 RSpec.describe SimpleMySQLPartitioning::Executor do
+  let(:table_name) { 'daily_reports' }
+
   describe '#add' do
     pending 'skip'
+  end
+
+  describe '#reorganize' do
+    let(:partiion_name) { 'p201808' }
+    let(:value)         { '2018-09-01' }
+    let(:instance)      { SimpleMySQLPartitioning::Executor.new(DailyReport, partiion_name) }
+
+    it 'has new partition' do
+      instance.reorganize(value, 'p999999')
+      expect(instance.exist?).to eq true
+    end
+  end
+
+  describe '#drop' do
+    let(:partiion_name) { 'p201808' }
+    let(:instance)      { SimpleMySQLPartitioning::Executor.new(DailyReport, partiion_name) }
+
+    it 'has new partition' do
+      instance.drop
+      expect(instance.exist?).to eq false
+    end
   end
 end
--- spec/simple_mysql_partitioning_spec.rb
+++ spec/simple_mysql_partitioning_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe SimpleMysqlParitioning do
   it 'has a version number' do
     expect(SimpleMysqlParitioning::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
