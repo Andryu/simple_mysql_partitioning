d97a4216fbddec9c44b5f9649cb2c57b78b07a4b
Shunsuke Andoh <shunsuke.andoh@gmail.com>
2018-07-30 01:18:43 +0900

    [feat] include Adapter

--- lib/simple_mysql_partitioning.rb
+++ lib/simple_mysql_partitioning.rb
@@ -1,9 +1,16 @@
 require 'simple_mysql_partitioning/version'
 require 'active_record'
 require 'mysql2'
+require 'simple_mysql_partitioning/adapter'
 require 'simple_mysql_partitioning/sql'
-require 'simple_mysql_partitioning/executor'
+require 'simple_mysql_partitioning/range'
+require 'simple_mysql_partitioning/base_partitioning'
 
-module SimpleMysqlParitioning
+module SimpleMySQLPartitioning
   # Your code goes here...
+  class << self
+    def included(klass)
+      klass.send :include, SimpleMySQLPartitioning::Adapter
+    end
+  end
 end
