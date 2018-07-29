64fe3c11976f669d8831d857f8a482e93ab1fa4a
Shunsuke Andoh <shunsuke.andoh@gmail.com>
2018-07-30 01:16:30 +0900

    [delete] files

--- lib/simple_mysql_partitioning/structure.rb
+++
@@ -1,9 +0,0 @@
-module SimpleMySQLPartitioning
-  class Structure
-    Config = Struct.new(:column, :by, :type) do
-      def initialize
-        super(column: column, by: by, type: type)
-      end
-    end
-  end
-end
